cf0116a64170941a5cde6b003c5cbf8788ea3d9b
keyru-web <keyr-syntax@gmail.com>
2024-12-28 22:43:30 +0300

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 const Task = require("../models/taskModel.js");
-const MONGODB_LOCAL = "mongodb://localhost:27017/taskmanager";
-const MONGODB_ATLAS =
-  "mongodb+srv://keyrumail:yopO9cm5F1gVyjoj@cluster0.w457g.mongodb.net/taskmanager?retryWrites=true&w=majority&appName=Cluster0";
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGODB_ATLAS);
+    const conn = await mongoose.connect(process.env.MONGODB_ATLAS);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
